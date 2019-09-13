f009e0b0a14451a902cf0f2c566ef49f6a73ef7f
Kirk Yang <neonmori@users.noreply.github.com>
2019-09-13 12:50:19 -0700

    Create bear.rb
    for bear 2.4.1

---
+++ Formula/bear.rb
@@ -0,0 +1,30 @@
+  
+class Bear < Formula
+  desc "Generate compilation database for clang tooling"
+  homepage "https://github.com/rizsotto/Bear"
+  url "https://github.com/rizsotto/Bear/archive/2.4.1.tar.gz"
+  sha256 "41e118471f11d91147490561b3bc52228a9ffc2a293e8e03717d674a0e312a9c"
+  head "https://github.com/rizsotto/Bear.git"
+
+  bottle do
+    cellar :any
+    sha256 "b540bfb4df22cda3cec0e2c1b3eb688ac34d67ed4b051063f05d44ff0c1da351" => :mojave
+    sha256 "19ffa59864de423dc7ae53767bb934f0355a77f3e5643cee73e5d4b84c073d8c" => :high_sierra
+    sha256 "0adda8dd7614d65451b1ea7fe36e3fd378672fc9ff828fbeb6ec0fb182d8ede7" => :sierra
+    sha256 "93e669ef1b8ab6c7d23a8120d1f7a6675d8893a0675994c35974df0732a5cb75" => :x86_64_linux
+  end
+
+  depends_on "cmake" => :build
+  depends_on "python"
+  depends_on "python@2" => :test unless OS.mac?
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/bear", "true"
+    assert_predicate testpath/"compile_commands.json", :exist?
+  end
+end
